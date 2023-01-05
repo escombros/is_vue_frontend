b46f0a788f332acbc280e5c2f5b8631326a92c66
IsraRamos <116392477+Jisraman@users.noreply.github.com>
2023-01-04 19:07:42 -0600

    first commit is

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import installElementPlus from './plugins/element'
+import VueSimpleContextMenu from 'vue-simple-context-menu';
+import 'vue-simple-context-menu/dist/vue-simple-context-menu.css';
+import './plugins/vee-validate'
+import axios from 'axios';
+
+
+axios.defaults.baseURL = "http://api-tecwebprod.escomproyectos.com/api/";
+const app = createApp(App)
+app.component('vue-simple-context-menu', VueSimpleContextMenu);
+installElementPlus(app)
+app.use(store).use(store).use(router).mount('#app')
